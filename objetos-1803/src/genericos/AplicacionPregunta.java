b9b00d97ddb29882e875d5cb47511e6ba4cba587
campitos <rapidclimate@gmail.com>
2018-06-07 19:21:42 -0500

    con ref cursor

---
+++ objetos-1803/src/genericos/AplicacionPregunta.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genericos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author campitos
+ */
+public class AplicacionPregunta {
+    
+    public static void main(String[] args) {
+        //Generamos las opciones
+        Opcion o11=new Opcion();
+              o11.setTitulo("Mizcalco");
+              o11.setCorrecta(false);
+              
+        Opcion o21=new Opcion();
+                o21.setTitulo("Texcoco");
+                o21.setCorrecta(false);
+                
+        Opcion o31=new Opcion();
+                o31.setTitulo("Madrid");
+                o31.setCorrecta(true);
+        
+        Opcion o41=new Opcion();
+                o41.setTitulo("Ecatepec");
+                o41.setCorrecta(false);   
+                //Sigue la pregunta
+            Pregunta p11=new Pregunta();
+            p11.setTitulo("¿Cual es la capital de España?");
+          //Antes generamos el arrayList con als opciones de arriba
+        ArrayList<Opcion> opcionesp11=new ArrayList<>();
+        opcionesp11.add(o11);
+        opcionesp11.add(o21);
+        opcionesp11.add(o31);
+        opcionesp11.add(o41);
+       //Ahora si este arrayList lo agregamos a la primer pregunta
+       p11.setOpciones(opcionesp11); 
+       
+       
+       //El cuestionario
+       Cuestionario c1=new Cuestionario();
+       ArrayList<Pregunta> preguntas=new ArrayList<>();
+       preguntas.add(p11);
+       c1.setPreguntas(preguntas);
+       
+       
+       for(Pregunta p:preguntas){
+           
+           System.out.println(p.getTitulo());
+       }
+       
+       
+    }
+    
+}
