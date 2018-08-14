df64c98a01ce20dad05ba4930c88b965ec4bfe4b
Juan Carlos Campos <rapidclimate@gmail.com>
2018-08-13 19:41:20 -0500

    vaaadinsillo

---
+++ InitializrSpringbootProject/src/main/java/org/campin/campin/CampinApplication.java
@@ -0,0 +1,12 @@
+package org.campin.campin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampinApplication.class, args);
+	}
+}
